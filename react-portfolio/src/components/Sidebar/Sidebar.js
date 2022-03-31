aaa76dec21a1e789a9da80ced215572c262166b1
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-03-31 14:54:24 -0400

    logos added but not displaying

--- react-portfolio/src/components/Sidebar/Sidebar.js
+++ react-portfolio/src/components/Sidebar/Sidebar.js
@@ -1,8 +1,8 @@
 import './Sidebar.scss'
-import { Link } from 'react-router-dom'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
-    
+import { Link, NavLink } from 'react-router-dom'
+
 const Sidebar = () => {
 return (
         <div className='nav-bar'>
