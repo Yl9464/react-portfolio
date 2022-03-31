00dff6f2c0c0deec1a068fdc78274d2d9aa6199a
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-03-31 15:54:11 -0400

    final sidebar commit

--- react-portfolio/src/components/Sidebar/Sidebar.js
+++ react-portfolio/src/components/Sidebar/Sidebar.js
@@ -2,11 +2,7 @@ import './Sidebar.scss'
 import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import {
-  faLinkedin,
-  faGithub,
-  faZoom,
-} from '@fortawesome/free-brands-svg-icons'
+import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons'
 import { faHome, faUser, faEnvelope } from '@fortawesome/free-solid-svg-icons'
 import { Link, NavLink } from 'react-router-dom'
 
