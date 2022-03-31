b65e35bf769cddb58891ba237685158bb3833c82
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-03-31 16:06:10 -0400

    started home page, create bottom tags

--- react-portfolio/src/components/Layout/Layout.js
+++ react-portfolio/src/components/Layout/Layout.js
@@ -3,10 +3,19 @@ import Sidebar from '../Sidebar/Sidebar'
 
 const Layout = () => {
     return (
-        <div className='App'>
-            <Sidebar />  
+    <div className="App">
+            <Sidebar />
+        <div className='page'>
+            <span className="tags top-tags">&lt;body&gt;</span>
+           
+
+            <span className="tags bottom-tags">
+             &lt;body&gt;
+            <br />
+            <span className='bottom-tag-html'>&lt;/html&gt;</span>
+            </span>
         </div>
-        
+    </div>
     )
 }
 
