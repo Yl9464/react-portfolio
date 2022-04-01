94303066fedd4b1e405d9424661fe976262dfb20
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-04-01 14:23:12 -0400

    added contact button

--- react-portfolio/src/components/Home/Home.js
+++ react-portfolio/src/components/Home/Home.js
@@ -1,11 +1,14 @@
-import './Home.scss'
+import './Home.scss';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
     return (
     <div className="container home-page"> 
         <div className='text-zone'>
             <h1> <u>Ying</u> <u>C.</u> <u>Lu</u> </h1>
-            <h2> Junior Software Engineer </h2>
+             <h2> Junior Software Engineer </h2>
+            <Link to='/contact' className='flat-button'>CONTACT ME</Link>
+               
         </div>
             
     </div>
