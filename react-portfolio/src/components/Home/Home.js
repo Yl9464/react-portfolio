9f1495d25101b15260be02c3ec7971de3b845333
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-04-01 15:12:20 -0400

    header animated, other text fadesin

--- react-portfolio/src/components/Home/Home.js
+++ react-portfolio/src/components/Home/Home.js
@@ -1,12 +1,33 @@
 import './Home.scss';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
+import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
 
 const Home = () => {
+    const [letterClass, setLetterClass] = useState('text-animate')
+    const nameArray = ['Y', 'i', 'n', 'g', ' ', 'C', '.', ' ', 'L', 'u']
+    const jobArray= ['J','u','n','i','o','r',' ','S','o','f','t','w','a','r','e',' ', 'E','n','g','i','n','e','e','r']
+
+    useEffect(() => {
+        return setTimeout(() => {
+            setLetterClass('text-animate-hover')
+        }, 4000)
+        }, [])
     return (
     <div className="container home-page"> 
         <div className='text-zone'>
-            <h1> <u>Ying</u> <u>C.</u> <u>Lu</u> </h1>
-             <h2> Junior Software Engineer </h2>
+                <h1>
+                <AnimatedLetters letterClass={letterClass}
+                    strArray={nameArray}
+                    idx={0}
+                    />
+                </h1>
+                <h2>
+                    <AnimatedLetters letterClass={letterClass}
+                    strArray={jobArray}
+                    idx={33}
+                    />
+                </h2>
             <Link to='/contact' className='flat-button'>CONTACT ME</Link>
                
         </div>
