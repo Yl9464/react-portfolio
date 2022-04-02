f216ca4163fda62d747c6f3b0f2019b1ae4a5b03
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-04-01 21:39:33 -0400

    added S logo animations

--- react-portfolio/src/components/Home/Home.js
+++ react-portfolio/src/components/Home/Home.js
@@ -2,6 +2,7 @@ import './Home.scss';
 import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import AnimatedLetters from '../AnimatedLetters/AnimatedLetters';
+import Logo from './Logo/Logo'
 
 const Home = () => {
     const [letterClass, setLetterClass] = useState('text-animate')
@@ -29,7 +30,7 @@ const Home = () => {
                     />
                 </h2>
             <Link to='/contact' className='flat-button'>CONTACT ME</Link>
-               
+            <Logo />       
         </div>
             
     </div>
