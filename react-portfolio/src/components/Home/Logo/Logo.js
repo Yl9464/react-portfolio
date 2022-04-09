f758677ab35e9835d141d36da2d62785a131d1dd
Yl9464 <86930915+Yl9464@users.noreply.github.com>
2022-04-09 15:30:43 -0400

    logos in flexbox

--- react-portfolio/src/components/Home/Logo/Logo.js
+++ react-portfolio/src/components/Home/Logo/Logo.js
@@ -1,3 +1,4 @@
+import './Logo.scss'
 import { useEffect, useRef } from 'react'
 import API from '../../../assets/images/API.png'
 import Chai from '../../../assets/images/Chai.png'
@@ -10,13 +11,28 @@ import Node from '../../../assets/images/Node.png'
 import Postman from '../../../assets/images/Postman.png'
 import React from '../../../assets/images/React.png'
 import Sequelize from '../../../assets/images/Sequelize.png'
-import AllLogos from '../../../assets/images/Logos.png'
-import './Logo.scss'
+//import AllLogos from '../../../assets/images/Logos.png'
+
 
 const Logo = () => {
   return (
-    <div className="Logos">
-      <img className='All-logos' src={AllLogos} alt='All-logos' />
+    <div className="Logos-container">
+      <div className="row-one">
+        <img className="API" src={API} alt="API-logo" />
+        <img className="Chai" src={Chai} alt="Chai-logo" />
+        <img className="CSS" src={CSS} alt="CSS-logo" />
+      </div>
+      <div className="row-two">
+        <img className="HTML" src={HTML} alt="HTML-logo" />
+        <img className="JavaScript" src={JavaScript} alt="JS-logo" />
+        <img className="Mocha" src={Mocha} alt="Mocha-logo" />
+      </div>
+      <div className="row-three">
+        <img className="MySQL" src={MySQL} alt="MySQL-logo" />
+        <img className="Node" src={Node} alt="Node-logo" />
+        <img className="Postman" src={Postman} alt="Postman-logo" />
+    
+      </div>
     </div>
   )
 }
